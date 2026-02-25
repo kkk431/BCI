05d741abea32668edd86f2f5a0ce4e83736a1ca4
kkk431 <hjxhjx1119@qq.com>
2026-02-25 17:45:35 +0800

    可视化入口

---
+++ core/visualizer/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+visualizer package
+多模态BCI平台可视化模块 - Tkinter版本
+"""
+
+from .signal_view import SignalView
+from .stats_view import StatsView
+from .bar_view import BarView
+from .plot_dialog import PlotDialog, quick_plot
+
+__all__ = [
+    'SignalView',
+    'StatsView',
+    'BarView',
+    'PlotDialog',
+    'quick_plot'
+]
